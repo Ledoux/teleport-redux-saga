f4dfa56e87be8a373ee85aaec8ecd059eff55acd
erwanledoux <lawanledoux@gmail.com>
2017-10-08 02:24:18 +0200

    removed bundler

--- bundler/onProdDone.js
+++
@@ -1,9 +0,0 @@
-require('babel-polyfill')
-require('babel-register')
-const serverSideRender = require('./serverSideRender.js').default
-
-function onProdDone (stats) {
-  serverSideRender(stats)
-}
-
-module.exports = onProdDone
